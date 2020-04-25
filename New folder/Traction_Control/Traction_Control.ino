76f06dbeaf18711d827015dd00e2ecaf0054fbe4
Tyler-Montcalm <montcalm-tyler@outlook.com>
2020-04-25 14:02:13 -0400

    almost deleted it all

---
+++ New folder/Traction_Control/Traction_Control.ino
@@ -0,0 +1,28 @@
+//Code written by: Tyler Montcalm
+#include "PID_traction.h"
+#include "definitions.h"
+
+
+float U_traction=0;
+float percent_speed=50;
+float my_motor_speed_traction=0;
+
+void launch_control();
+
+void loop() 
+{
+  // Idea is to adjust the speed of the rear so that the front and back wheels are reading an equivalent speed i.e no slip
+  my_motor_speed_traction=(10*percent_speed)+U_traction;
+  my_motor(my_motor_speed_traction);
+  U_traction=pid_traction_calculate(percent_speed); // this will return a U
+  
+}
+
+void launch_control()
+{
+  // for launch control we ramp up voltage until there is slip and then stay there so essentially 
+  //its a contrinuous PID where we are accellerating as fast as possible same thing as traction control
+  my_motor_speed_traction=(10*percent_speed)+U_traction;
+  my_motor(my_motor_speed_traction);
+  U_traction=pid_traction_calculate(percent_speed); // this will return a U
+}
