340b273070c1bc222f20c229ee259caa77918ff8
Tyler-Montcalm <montcalm-tyler@outlook.com>
2020-04-28 16:44:02 -0400

    Working two value register analog_read

--- Register_Level_Analog/Register_Level_Analog/Register_Level_Analog.ino
+++ Register_Level_Analog/Register_Level_Analog/Register_Level_Analog.ino
@@ -9,6 +9,7 @@
 float adc_analog_read(int analog_port);
 float v_a0,v_a1,v_a2;
 float adc_analog_avg(int analog_port);
+void setup_reccuring();
 void setup() 
 {
   Serial.begin(115200);
@@ -35,8 +36,11 @@ void setup()
 
 void loop()
 { 
-  Serial.println(adc_analog_avg(1));
-   //delay(300);
+   Serial.println(adc_analog_read(1));
+   delay(300);
+   Serial.println(adc_analog_read(0));
+   delay(300);
+   setup_reccuring();
 }
 float adc_analog_read(int analog_port)
 {
@@ -49,13 +53,8 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
-  t2 = micros();// ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
- // Serial.print("time to complete analog_read_register = ");
-  //Serial.println(dt);
   return Vin;
   }
   if(analog_port==1)
@@ -64,9 +63,6 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  t2 = micros(); // ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
   return Vin;
@@ -77,9 +73,6 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  t2 = micros(); // ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
   return Vin;
@@ -90,13 +83,8 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
-  t2 = micros();// ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
- // Serial.print("time to complete analog_read_register = ");
-  //Serial.println(dt);
   return Vin;
   }
   if(analog_port==4)
@@ -105,9 +93,6 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  t2 = micros(); // ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
   return Vin;
@@ -118,16 +103,33 @@ float adc_analog_read(int analog_port)
   ADCSRA |= BIT(ADSC);
   while( ADCSRA & BIT(ADSC) ) i++;
   adc1 = ADC;
-  t2 = micros(); // ADC conversion completion time
-  dt = t2 - t1; // time for ADC conversion
-  // ADC = Vin*1023/Vref
   Vref = 5.0;
   Vin  = adc1/1023.0*Vref;
   return Vin;
   }
  
 }
+void setup_reccuring()
+{
+  cli(); // stop all interrupt functions
 
+  //Select ADC Channel by uncommenting. be careful about changing channel during conversion process
+  //uncomment as needed for A0 to A5
+  ADMUX =0;    //Initially set mux register to zero A0
+  //ADMUX |= BIT(MUX0); //A1
+  //ADMUX |= BIT(MUX1); //A2
+  //ADMUX |= BIT(MUX0) | BIT(MUX1); //A3
+  //ADMUX |= BIT(MUX2); //A4
+  //ADMUX |= BIT(MUX0) | BIT(MUX2); //A5
+  ADMUX |= BIT(REFS0); // sets your ADC reference to 5V cause default is Aref pin
+  ADCSRA=0; // sets your ADC Control and Status Register A to zero
+  ADCSRA |= BIT(ADEN); //ADC enable on status register A
+  //use a 128 prescaler (slowest)
+  ADCSRA |= BIT(ADPS0) | BIT(ADPS1) | BIT(ADPS2);
+  ADCSRA |= BIT(ADSC); // start ADC conversion
+  //BIT(ADSC) will be 1 whenever a conversion is happening and be 0 when complete to read a bit you and it
+  sei();
+}
 float adc_analog_avg(int analog_port)
 {
   for(int i=0; i<50; i++)
@@ -135,7 +137,5 @@ float adc_analog_avg(int analog_port)
     v_a0+=adc_analog_read(analog_port);
   }
   v_a0=v_a0/50;
- // Serial.print("reading = ");
-   //Serial.println(v_a0);
    return v_a0;
 }
